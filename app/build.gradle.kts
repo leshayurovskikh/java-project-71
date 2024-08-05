3012a8ebf86364e4b2d0282df7382ede7d636a6c
Aleksey <alexei./yurovskikh@yandex.ru>
2024-08-05 23:37:01 +0500

    Step 1,2

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+application { mainClass.set("hexlet.code.App") }
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
