71708ec7e2cbacbb7a2ddfa6e811f375e513dd61
Aleksey Yurovskikh <alexei.yurovskikh@yandex.ru>
2024-08-08 20:58:55 +0300

    Add checkstyle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     application
     checkstyle
+    jacoco
 }
 
 group = "hexlet.code"
@@ -12,10 +13,14 @@ repositories {
 }
 application { mainClass.set("hexlet.code.App") }
 dependencies {
+    implementation("info.picocli:picocli:4.7.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+tasks.jacocoTestReport {
+    reports { xml.required.set(true)}
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,8 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+
+@CommandLine.Command
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
