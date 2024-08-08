6366602dc276c490b9a00a1e8a5ab2fa5486e5d5
Aleksey Yurovskikh <alexei.yurovskikh@yandex.ru>
2024-08-08 21:14:36 +0300

    Add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,9 +2,18 @@ package hexlet.code;
 
 import picocli.CommandLine;
 
-@CommandLine.Command
-public class App {
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "Demo", mixinStandardHelpOptions = true)
+public class App implements Callable<Integer> {
+@Override
+public Integer call() throws Exception {
+    return 0;
+}
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App ()).execute(args);
+        System.exit(exitCode);;
     }
 }
