60ecf41c962aeebcd08511833870a22bc26355a8
Aleksey Yurovskikh <alexei.yurovskikh@yandex.ru>
2024-08-09 18:06:04 +0300

    Add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,8 +4,21 @@ import picocli.CommandLine;
 
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "Demo", mixinStandardHelpOptions = true)
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true)
 public class App implements Callable<Integer> {
+    @CommandLine.Parameters(index = "0",paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @CommandLine.Parameters(index = "1",paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @CommandLine.Option (
+            names = {"-f", "--format"},
+            paramLabel = "format",
+            defaultValue = "stylish",
+            description = "output format [default: stylish]"
+    )
+    private String format;
 @Override
 public Integer call() throws Exception {
     return 0;
@@ -14,6 +27,6 @@ public Integer call() throws Exception {
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App ()).execute(args);
-        System.exit(exitCode);;
+        System.exit(exitCode);
     }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,8 @@
+package hexlet.code;
+
+public class Differ {
+    public static String geberate(String a, String b){
+        String result = String.valueOf (a.equals (b));
+        return result;
+    }
+}
