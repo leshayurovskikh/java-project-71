427608af944610e33595b45cc0f1b11df481d4a2
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-09-07 12:36:52 +0500

    Update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -10,31 +10,31 @@ import java.util.Map;
 
 public class Differ {
     public static String generate(String filepath1, String filepath2, String format) throws Exception {
-        String content1 = readFile (filepath1);
-        String content2 = readFile (filepath2);
+        String content1 = readFile(filepath1);
+        String content2 = readFile(filepath2);
 
-        String fileFormat1 = getFileType (filepath1);
-        String fileFormat2 = getFileType (filepath2);
+        String fileFormat1 = getFileType(filepath1);
+        String fileFormat2 = getFileType(filepath2);
 
-       Map<String, Object> file1 = Parser.parse (content1, fileFormat1);
-       Map<String, Object> file2 = Parser.parse (content2, fileFormat2);
+        Map<String, Object> file1 = Parser.parse(content1, fileFormat1);
+        Map<String, Object> file2 = Parser.parse(content2, fileFormat2);
 
-        List<Map<String, Object>> compareResult = Comparator.compare (file1,file2);
+        List<Map<String, Object>> compareResult = Comparator.compare(file1, file2);
 
-        return Formatter.format (compareResult, format);
+        return Formatter.format(compareResult, format);
 
     }
     public static String generate(String filepath1, String filepath2) throws Exception {
         return generate(filepath1, filepath2, "stylish");
     }
 
-public static String readFile(String filepath) throws Exception {
-    Path path = Paths.get(filepath).toAbsolutePath().normalize();
-    if (!Files.exists(path)) {
-        throw new Exception("File '" + path + "' does not exist");
-    }
-    String content = Files.readString(path);
-    return content;
+    public static String readFile(String filepath) throws Exception {
+        Path path = Paths.get(filepath).toAbsolutePath().normalize();
+            if (!Files.exists(path)) {
+                throw new Exception("File '" + path + "' does not exist");
+            }
+        String content = Files.readString(path);
+        return content;
 }
 
 
