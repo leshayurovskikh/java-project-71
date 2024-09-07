8b099eae194a8c91d7c1aa631ed98db68d94abdf
leshayurovskikh <155832943+leshayurovskikh@users.noreply.github.com>
2024-09-07 13:02:11 +0500

    Update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,4 @@
 package hexlet.code;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
